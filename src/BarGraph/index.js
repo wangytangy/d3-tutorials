dad5337c2db2eb1aeb8852d31a1053ff8c538983
wangytangy <yue.bruce.wang@gmail.com>
2018-08-13 23:07:35 -0400

    Create scaled bar graph, add CSS reset file

--- src/BarGraph/index.js
+++ src/BarGraph/index.js
@@ -1,17 +1,32 @@
 var data = [30, 86, 168, 281, 303, 365];
 
 const renderBarGraph = () => {
-  return d3.select(".chart")
+  return d3.select(".fixed-scale")
     .selectAll("div")
     .data(data)
-    .enter()
-    .append("div")
+  .enter().append("div")
     .style("width", function(d) { return d + "px"; })
     .text(function(d) { return d; });
 }
 
+const scale = d3.scaleLinear()
+.domain([0, d3.max(data)])
+.range([0, 300])
+
+const renderLinearScaleBarGraph = () => {
+  return d3.select(".linear-scale")
+    .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return scale(d) + "px"; })
+    .style('padding', 0)
+    .style('margin', '1px 0 1px 0')
+    .text(function(d) { return d; });
+}
+
 const BarGraphModule = {
-  renderBarGraph
+  renderBarGraph,
+  renderLinearScaleBarGraph
 }
 
 export default BarGraphModule;
--- src/app.js
+++ src/app.js
@@ -2,3 +2,4 @@ import './assets/scss/app.scss';
 import BarGraphModule from './BarGraph/index';
 
 BarGraphModule.renderBarGraph();
+BarGraphModule.renderLinearScaleBarGraph();
